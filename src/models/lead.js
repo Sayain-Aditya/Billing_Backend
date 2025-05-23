7208cbb02572d454c29b31f2a1eeb60c76ba51c1
Sayain-Aditya <as4316664@gmail.com>
2025-05-23 17:41:37 +0530

    ver

---
+++ src/models/lead.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const leadSchema = new mongoose.Schema({
+     name:{},
+    email: "",
+    phone: "",
+    Address: "",
+    enquiry: "Select Enquiry",
+    followUpDate: "",
+    followUpStatus: "pending",
+    meetingdate: "",
+    status: "true",
+    calldate: "",
+    update: "",
+    notes:
+})
