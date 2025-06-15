ca010d4cbd4ad4195e9af2ad7c700de461a96396
Sayain-Aditya <as4316664@gmail.com>
2025-06-15 23:44:02 +0530

    SIZE

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -7,7 +7,7 @@ const storage = multer.memoryStorage();
 // Define the multer upload middleware (handles up to 10 files at a time)
 const upload = multer({
   storage: storage,
-limits: { fileSize: 10 * 1024 * 1024 },  // 10MB
+limits: { fileSize: 0.5 * 1024 * 1024 },  // 500KB limit for each file
 
 }).array('images', 20);  // 'images' is the field name in the form, and 10 is the maximum number of files
 
